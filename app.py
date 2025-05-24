2ba69c78e8a01ae38927408e73dea3058ce92017
verge88 <157506627+verge88@users.noreply.github.com>
2025-05-24 19:29:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from scraper import MeganormScraper
 import logging
@@ -113,4 +114,5 @@ def internal_error(error):
     }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
